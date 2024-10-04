85a74e9ed1bfcee81d5be519ca8ab14a14bb4b6b
Ricardo Sueiras <ricardo.sueiras@gmail.com>
2024-10-04 18:48:36 +0100

    working app

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:change-me@127.0.0.1/voting'
+
+
+# Construct the database URI using environment variables
+db_user = os.environ.get('DB_USER', 'postgres')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST', '127.0.0.1')
+db_name = os.environ.get('DB_NAME', 'voting')
+
+if not db_password:
+    raise ValueError("Database password not set in environment variables")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
 
